ef1ebe532c4ed7ac585beb5da0c4d59094461eb1
Chazzychouse <acecartwright101@hotmail.com>
2022-08-09 18:35:35 -0400

    Added Ranked Details

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import React, { useState } from "react";
 function App() {
   const [searchText, setSearchText] = useState("");
   const [playerData, setPlayerData] = useState({});
+  const [moreData, setMoreData] = useState({});
   const API_KEY = "RGAPI-1feb4546-c319-4568-a975-c8eac306985e";
 
   function searchForPlayer(event) {
@@ -28,7 +29,27 @@ function App() {
       });
   }
 
-  console.log(playerData);
+  let summoner_id = playerData.id;
+  let summonerString = "" + summoner_id;
+
+  function getPlayerDetails(event) {
+    var detailCallString =
+      "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/" +
+      summonerString +
+      "?api_key=" +
+      API_KEY;
+    axios
+      .get(detailCallString)
+      .then(function (response) {
+        //success
+        setMoreData(response.data);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        //Error
+        console.log(error);
+      });
+  }
 
   return (
     <div className="App">
@@ -37,26 +58,48 @@ function App() {
         <input type="text" onChange={(e) => setSearchText(e.target.value)} />
         <button onClick={(e) => searchForPlayer(e)}>Search</button>
       </div>
-      {JSON.stringify(playerData) != "{}" ? (
-        <>
-          <p>{playerData.name}</p>
-          <p>Summoner Level: {playerData.summonerLevel}</p>
-          <img
-            height="100"
-            width="100"
-            src={
-              "http://ddragon.leagueoflegends.com/cdn/12.14.1/img/profileicon/" +
-              playerData.profileIconId +
-              ".png"
-            }
-            alt="a"
-          />
-        </>
-      ) : (
-        <>
-          <p>No player data</p>
-        </>
-      )}
+      <div id="loaded-content">
+        {JSON.stringify(playerData) != "{}" ? (
+          <>
+            <p id="player-name">{playerData.name}</p>
+            <div className="break"></div>
+            <p id="player-level">Summoner Level: {playerData.summonerLevel}</p>
+            <div className="break"></div>
+            <img
+              height="100"
+              width="100"
+              id="icon"
+              src={
+                "http://ddragon.leagueoflegends.com/cdn/12.14.1/img/profileicon/" +
+                playerData.profileIconId +
+                ".png"
+              }
+              alt="a"
+            />
+            <div className="break"></div>
+            <button id="details" onClick={(e) => getPlayerDetails(e)}>
+              More Details
+            </button>
+            <div className="break"></div>
+          </>
+        ) : (
+          <>
+            <p>No player data</p>
+          </>
+        )}
+      </div>
+      <div id="loaded-details">
+        {JSON.stringify(moreData) != "{}" ? (
+          <>
+            <p>Rank: {moreData[0].tier + " " + moreData[0].rank}</p>
+            <p>
+              Wins: {moreData[0].wins} Losses: {moreData[0].losses}
+            </p>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
